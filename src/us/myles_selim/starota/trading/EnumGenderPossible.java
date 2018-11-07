227d1a3056237791eafcc344e4ab0f1c554aecb6
Myles Selim <Selim042@users.noreply.github.com>
2018-11-06 21:32:19 -0600

    Update EnumGenderPossible for proper embeds

--- src/us/myles_selim/starota/trading/EnumGenderPossible.java
+++ src/us/myles_selim/starota/trading/EnumGenderPossible.java
@@ -2,7 +2,23 @@ package us.myles_selim.starota.trading;
 
 public enum EnumGenderPossible {
 	UNKNOWN,
-	EITHER,
+	EITHER(
+			"male/female"),
 	MALE,
 	FEMALE;
+
+	private String display;
+
+	EnumGenderPossible() {
+		this(null);
+	}
+
+	EnumGenderPossible(String display) {
+		this.display = display;
+	}
+
+	@Override
+	public String toString() {
+		return this.display == null ? this.name() : this.display;
+	}
 }
