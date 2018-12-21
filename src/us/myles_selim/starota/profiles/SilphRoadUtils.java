f1f369912425cccd0b0dc4416aa74ed77bbcc67f
Myles Selim <Selim042@users.noreply.github.com>
2018-12-20 20:55:00 -0600

    Cache Silph Road API info, version bump

--- src/us/myles_selim/starota/profiles/SilphRoadUtils.java
+++ src/us/myles_selim/starota/profiles/SilphRoadUtils.java
@@ -5,17 +5,28 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.HashMap;
+import java.util.Map;
 
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+
+import us.myles_selim.starota.CachedData;
 
 public class SilphRoadUtils {
 
 	private static final String API_URL = "https://sil.ph/";
 	private static final JsonParser PARSER = new JsonParser();
 
+	private static final Map<String, CachedData<Boolean>> CARD_CACHE = new HashMap<>();
+
 	public static boolean hasCard(String pogoName) {
+		if (CARD_CACHE.containsKey(pogoName)) {
+			CachedData<Boolean> cData = CARD_CACHE.get(pogoName);
+			if (!cData.hasPassed(60000)) // 10 mins
+				return cData.getValue();
+		}
 		try {
 			URL url = new URL(API_URL + pogoName + ".json");
 			URLConnection conn = url.openConnection();
@@ -25,7 +36,9 @@ public class SilphRoadUtils {
 			while ((line = in.readLine()) != null)
 				json += line;
 			JsonObject root = PARSER.parse(json).getAsJsonObject();
-			return root.get("data") != null;
+			boolean hasCard = root.get("data") != null;
+			CARD_CACHE.put(pogoName, new CachedData<>(hasCard));
+			return hasCard;
 		} catch (IOException e) {
 			return false;
 		}
