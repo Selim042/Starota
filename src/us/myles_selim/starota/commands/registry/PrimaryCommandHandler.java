2d91a98efb36628d3ba388d27ecb0b74249b6023
Myles Selim <Selim042@users.noreply.github.com>
2019-01-06 10:25:21 -0600

    Add command suggestions when a command is not found

--- src/us/myles_selim/starota/commands/registry/PrimaryCommandHandler.java
+++ src/us/myles_selim/starota/commands/registry/PrimaryCommandHandler.java
@@ -1,6 +1,7 @@
 package us.myles_selim.starota.commands.registry;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
@@ -13,8 +14,10 @@ import sx.blah.discord.handle.impl.obj.ReactionEmoji;
 import sx.blah.discord.handle.obj.IChannel;
 import sx.blah.discord.handle.obj.IGuild;
 import sx.blah.discord.handle.obj.IMessage;
+import sx.blah.discord.util.EmbedBuilder;
 import sx.blah.discord.util.RequestBuffer;
 import us.myles_selim.starota.Starota;
+import us.myles_selim.starota.commands.registry.channel_management.ChannelCommandManager;
 import us.myles_selim.starota.modules.StarotaModule;
 import us.myles_selim.starota.wrappers.StarotaServer;
 
@@ -25,6 +28,7 @@ public class PrimaryCommandHandler {
 
 	public static final String PREFIX_KEY = "cmd_prefix";
 	public static final String DEFAULT_CATEGORY = "Uncategorized";
+	public static final int DEFAULT_SUGGESTION_COUNT = 10;
 
 	public static String getPrefix(IGuild guild) {
 		StarotaServer server = StarotaServer.getServer(guild);
@@ -60,11 +64,15 @@ public class PrimaryCommandHandler {
 		});
 		if (Starota.DEBUG)
 			message.addReaction(ReactionEmoji.of("�?"));
+		boolean cmdFound = false;
 		try {
 			for (ICommandHandler h : COMMAND_HANDLERS)
-				if (h.executeCommand(args, message, server, channel))
+				if (h.executeCommand(args, message, server, channel)) {
+					cmdFound = true;
 					continue;
+				}
 		} catch (Throwable e) {
+			cmdFound = true;
 			RequestBuffer.request(() -> {
 				message.reply("There was an error encountered while executing your command: "
 						+ e.getClass().getName() + ": " + e.getLocalizedMessage() + "\n"
@@ -74,6 +82,18 @@ public class PrimaryCommandHandler {
 			e.printStackTrace();
 			if (!(e instanceof LuaError))
 				Starota.submitError(" on server " + server.getName(), e);
+		}
+		if (!cmdFound) {
+			EmbedBuilder builder = new EmbedBuilder();
+			builder.withTitle("Did you mean...?");
+			for (ICommand cmd : getSuggestions(server, channel, args[0], 5)) {
+				if (cmd != null) {
+					String desciption = cmd.getDescription();
+					builder.appendDesc("- " + prefix + cmd.getName()
+							+ (desciption == null ? "" : ", _" + cmd.getDescription() + "_") + "\n");
+				}
+			}
+			channel.sendMessage(builder.build());
 		}
 		RequestBuffer.request(() -> {
 			channel.setTypingStatus(false);
@@ -107,6 +127,88 @@ public class PrimaryCommandHandler {
 				return c;
 		}
 		return null;
+	}
+
+	public static ICommand[] getSuggestions(IGuild server, String input) {
+		return getSuggestions(server, null, input, DEFAULT_SUGGESTION_COUNT);
+	}
+
+	public static ICommand[] getSuggestions(IGuild server, String input, int count) {
+		return getSuggestions(server, null, input, count);
+	}
+
+	public static ICommand[] getSuggestions(IGuild server, IChannel channel, String input) {
+		return getSuggestions(server, channel, input, DEFAULT_SUGGESTION_COUNT);
+	}
+
+	public static ICommand[] getSuggestions(IGuild server, IChannel channel, String input, int count) {
+		List<DistancedCommand> suggestions = new ArrayList<>();
+		for (ICommandHandler ch : COMMAND_HANDLERS) {
+			for (ICommand cmd : ch.getAllCommands(server)) {
+				if (channel != null && !ChannelCommandManager
+						.isAllowedHere(StarotaServer.getServer(server), cmd.getCategory(), channel))
+					continue;
+				for (String a : cmd.getAliases()) {
+					DistancedCommand dc = new DistancedCommand(calculateDistance(a, input), cmd);
+					if (!suggestions.contains(dc))
+						suggestions.add(dc);
+				}
+			}
+		}
+		suggestions.sort(null);
+		ICommand[] out = new ICommand[count];
+		for (int i = 0; i < count; i++)
+			out[i] = suggestions.get(i).cmd;
+		return out;
+	}
+
+	private static class DistancedCommand implements Comparable<DistancedCommand> {
+
+		public int dist;
+		public ICommand cmd;
+
+		public DistancedCommand(int dist, ICommand cmd) {
+			this.dist = dist;
+			this.cmd = cmd;
+		}
+
+		@Override
+		public int compareTo(DistancedCommand o) {
+			return Integer.compare(dist, o.dist);
+		}
+
+		@Override
+		public boolean equals(Object o) {
+			if (!(o instanceof DistancedCommand))
+				return false;
+			return cmd.equals(((DistancedCommand) o).cmd);
+		}
+	}
+
+	private static int calculateDistance(String x, String y) {
+		int[][] dp = new int[x.length() + 1][y.length() + 1];
+		for (int i = 0; i <= x.length(); i++) {
+			for (int j = 0; j <= y.length(); j++) {
+				if (i == 0) {
+					dp[i][j] = j;
+				} else if (j == 0) {
+					dp[i][j] = i;
+				} else {
+					dp[i][j] = min(
+							dp[i - 1][j - 1] + costOfSubstitution(x.charAt(i - 1), y.charAt(j - 1)),
+							dp[i - 1][j] + 1, dp[i][j - 1] + 1);
+				}
+			}
+		}
+		return dp[x.length()][y.length()];
+	}
+
+	private static int costOfSubstitution(char a, char b) {
+		return a == b ? 0 : 1;
+	}
+
+	private static int min(int... numbers) {
+		return Arrays.stream(numbers).min().orElse(Integer.MAX_VALUE);
 	}
 
 	private static String[] getArgs(IMessage message, IGuild guild) {
